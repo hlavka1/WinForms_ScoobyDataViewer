3cd142fd4b36d7594d1b5b4128c3d6fb9bcb12d7
Justina L Hlavka <justinahlavka@yahoo.com>
2018-10-26 20:38:21 -0400

    Add files via upload

---
+++ Demo_WinForms_FlintstonesViewer/DetailForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WinForms_DataViewer
+{
+    partial class DetailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox_Photo = new System.Windows.Forms.PictureBox();
+            this.lbl_FullName = new System.Windows.Forms.Label();
+            this.lbl_Age = new System.Windows.Forms.Label();
+            this.lbl_Gender = new System.Windows.Forms.Label();
+            this.lbl_Description = new System.Windows.Forms.Label();
+            this.btn_Close = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Photo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox_Photo
+            // 
+            this.picBox_Photo.Location = new System.Drawing.Point(24, 76);
+            this.picBox_Photo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.picBox_Photo.Name = "picBox_Photo";
+            this.picBox_Photo.Size = new System.Drawing.Size(210, 264);
+            this.picBox_Photo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox_Photo.TabIndex = 3;
+            this.picBox_Photo.TabStop = false;
+            // 
+            // lbl_FullName
+            // 
+            this.lbl_FullName.AutoSize = true;
+            this.lbl_FullName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_FullName.Location = new System.Drawing.Point(17, 11);
+            this.lbl_FullName.Name = "lbl_FullName";
+            this.lbl_FullName.Size = new System.Drawing.Size(164, 37);
+            this.lbl_FullName.TabIndex = 4;
+            this.lbl_FullName.Text = "Full Name";
+            // 
+            // lbl_Age
+            // 
+            this.lbl_Age.AutoSize = true;
+            this.lbl_Age.Location = new System.Drawing.Point(272, 105);
+            this.lbl_Age.Name = "lbl_Age";
+            this.lbl_Age.Size = new System.Drawing.Size(38, 20);
+            this.lbl_Age.TabIndex = 5;
+            this.lbl_Age.Text = "Age";
+            // 
+            // lbl_Gender
+            // 
+            this.lbl_Gender.AutoSize = true;
+            this.lbl_Gender.Location = new System.Drawing.Point(272, 148);
+            this.lbl_Gender.Name = "lbl_Gender";
+            this.lbl_Gender.Size = new System.Drawing.Size(63, 20);
+            this.lbl_Gender.TabIndex = 6;
+            this.lbl_Gender.Text = "Gender";
+            // 
+            // lbl_Description
+            // 
+            this.lbl_Description.Location = new System.Drawing.Point(49, 344);
+            this.lbl_Description.Name = "lbl_Description";
+            this.lbl_Description.Size = new System.Drawing.Size(536, 407);
+            this.lbl_Description.TabIndex = 7;
+            this.lbl_Description.Text = "Description";
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(276, 774);
+            this.btn_Close.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(84, 35);
+            this.btn_Close.TabIndex = 8;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DetailForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 832);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.lbl_Description);
+            this.Controls.Add(this.lbl_Gender);
+            this.Controls.Add(this.lbl_Age);
+            this.Controls.Add(this.lbl_FullName);
+            this.Controls.Add(this.picBox_Photo);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "DetailForm";
+            this.Text = "DetailForm";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.DetailForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Photo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picBox_Photo;
+        private System.Windows.Forms.Label lbl_FullName;
+        private System.Windows.Forms.Label lbl_Age;
+        private System.Windows.Forms.Label lbl_Gender;
+        private System.Windows.Forms.Label lbl_Description;
+        private System.Windows.Forms.Button btn_Close;
+    }
+}
---
+++ Demo_WinForms_FlintstonesViewer/ListForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WinForms_DataViewer
+{
+    partial class ListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView_Characters = new System.Windows.Forms.DataGridView();
+            this.btn_DeleteSelected = new System.Windows.Forms.Button();
+            this.btn_DetailView = new System.Windows.Forms.Button();
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_sort = new System.Windows.Forms.Button();
+            this.btn_sort2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Characters)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_Characters
+            // 
+            this.dataGridView_Characters.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Characters.Location = new System.Drawing.Point(39, 79);
+            this.dataGridView_Characters.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView_Characters.Name = "dataGridView_Characters";
+            this.dataGridView_Characters.RowTemplate.Height = 24;
+            this.dataGridView_Characters.Size = new System.Drawing.Size(916, 286);
+            this.dataGridView_Characters.TabIndex = 0;
+            // 
+            // btn_DeleteSelected
+            // 
+            this.btn_DeleteSelected.Location = new System.Drawing.Point(50, 396);
+            this.btn_DeleteSelected.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_DeleteSelected.Name = "btn_DeleteSelected";
+            this.btn_DeleteSelected.Size = new System.Drawing.Size(127, 56);
+            this.btn_DeleteSelected.TabIndex = 1;
+            this.btn_DeleteSelected.Text = "Delete Selected";
+            this.btn_DeleteSelected.UseVisualStyleBackColor = true;
+            this.btn_DeleteSelected.Click += new System.EventHandler(this.btn_CheckList_Click);
+            // 
+            // btn_DetailView
+            // 
+            this.btn_DetailView.Location = new System.Drawing.Point(247, 394);
+            this.btn_DetailView.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_DetailView.Name = "btn_DetailView";
+            this.btn_DetailView.Size = new System.Drawing.Size(122, 60);
+            this.btn_DetailView.TabIndex = 2;
+            this.btn_DetailView.Text = "View Detail";
+            this.btn_DetailView.UseVisualStyleBackColor = true;
+            this.btn_DetailView.Click += new System.EventHandler(this.btn_DetailView_Click);
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Title.Location = new System.Drawing.Point(200, 11);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(581, 40);
+            this.lbl_Title.TabIndex = 3;
+            this.lbl_Title.Text = "Characters from Scooby Doo Show";
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(826, 396);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(122, 60);
+            this.btn_exit.TabIndex = 4;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // btn_sort
+            // 
+            this.btn_sort.Location = new System.Drawing.Point(432, 394);
+            this.btn_sort.Name = "btn_sort";
+            this.btn_sort.Size = new System.Drawing.Size(129, 61);
+            this.btn_sort.TabIndex = 5;
+            this.btn_sort.Text = "Sort By Age";
+            this.btn_sort.UseVisualStyleBackColor = true;
+            this.btn_sort.Click += new System.EventHandler(this.btn_sort_Click);
+            // 
+            // btn_sort2
+            // 
+            this.btn_sort2.Location = new System.Drawing.Point(628, 396);
+            this.btn_sort2.Name = "btn_sort2";
+            this.btn_sort2.Size = new System.Drawing.Size(128, 59);
+            this.btn_sort2.TabIndex = 6;
+            this.btn_sort2.Text = "Sort By Name";
+            this.btn_sort2.UseVisualStyleBackColor = true;
+            this.btn_sort2.Click += new System.EventHandler(this.btn_sort2_Click);
+            // 
+            // ListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 482);
+            this.Controls.Add(this.btn_sort2);
+            this.Controls.Add(this.btn_sort);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.lbl_Title);
+            this.Controls.Add(this.btn_DetailView);
+            this.Controls.Add(this.btn_DeleteSelected);
+            this.Controls.Add(this.dataGridView_Characters);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "ListForm";
+            this.Text = "Cartoon Characters";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Characters)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_Characters;
+        private System.Windows.Forms.Button btn_DeleteSelected;
+        private System.Windows.Forms.Button btn_DetailView;
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_sort;
+        private System.Windows.Forms.Button btn_sort2;
+    }
+}
+
